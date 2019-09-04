1a5ceb8458714d1e5a349dfbc1b7793f5b7917c2
comic-engineer-3780 <ethanjosephfournier@gmail.com>
2019-09-04 23:35:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,9 +8,8 @@ def map(source_array)
 end
 
 def reduce(source_array,starting_point = 0)
-  result = 0
   source_array.length.times { |i|
-    result = yield(source_array[i])
+    starting_point = yield(source_array[i])
   }
-  result
+  starting_point
 end
