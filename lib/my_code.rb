a62b7bbf69faecee65b5c9a1eb9ccebea9be01bb
comic-engineer-3780 <ethanjosephfournier@gmail.com>
2019-09-05 01:03:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,16 +9,16 @@ end
 
 def reduce(source_array,starting_point = nil)
   if starting_point
-    num1 = starting_point
+    sum = starting_point
     i = 0
   else
-    num1 = source_array[0]
+    sum = source_array[0]
     i = 1
   end
     
   while i < source_array.length
-    num1 = yield(num1,source_array[i])
+    sum = yield(sum,source_array[i])
     i += 1
   end
-  starting_point
+  sum
 end
